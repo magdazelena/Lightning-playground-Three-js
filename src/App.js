1b0db616ba8ab56dfb46523eccaa032dc804e2e6
Magda Zelezik <magda.zelezik@gmail.com>
2021-07-08 16:23:57 +0200

    Updated readme and removed usused dependency

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import THREE from './3D/three';
+const App = () => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-function App() {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+  const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+  scene.add( light );
+  camera.position.z = 5;
+
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper)
+  controls.update()
+
+  const animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    controls.update()
+    renderer.render( scene, camera );
+  };
+
+  animate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
