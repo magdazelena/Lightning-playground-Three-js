28c3d4c7e3624dbeac01b0c4438a1a15e4563463
magdazelena <magda.zelezik@gmail.com>
2021-12-04 16:46:35 +0100

    Added single choice to the menu

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Shape from './3D/objects';
 import shapes from './3D/objects/shapes.json'
 const App = () => {
   const [selection, setSelection] = useState([])
-  const [shapeSelection, setShapeSelection] = useState([])
+  const [shapeSelection, setShapeSelection] = useState('')
   extend({ OrbitControls })
   const CameraControls = () => {
     const {
@@ -23,12 +23,12 @@ const App = () => {
   return (
     <>
       <Menu items={lights} orientation='horizontal' onChange={setSelection} />
-      <Menu items={shapes} orientation='vertical' onChange={setShapeSelection} />
+      <Menu items={shapes} singleChoice={true} orientation='vertical' onChange={setShapeSelection} />
       <div className="background">
         <Canvas style={{ background: color.purple }}>
           <CameraControls />
           <Lights selection={selection} />
-          <Shape type={shapeSelection[0]} color={color.lavender} color2={color.violet} />
+          <Shape type={shapeSelection} color={color.lavender} color2={color.violet} />
         </Canvas>
       </div>
     </>
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './index.css'
 const Menu = (props) => {
-	const { onChange, items, orientation } = props
+	const { onChange, items, orientation, singleChoice } = props
 	const [checkedList, setCheckedList] = useState([])
-
-	const toggleCheck = (e) => {
+	const [checkedChoice, setCheckedChoice] = useState('')
+	const toggleChecklist = (e) => {
 		if (checkedList.includes(e.target.id)) {
 			setCheckedList(prevState => prevState.filter(item => item !== e.target.id))
 		} else {
 			setCheckedList(prevState => [...prevState, e.target.id])
 		}
 	}
+	const toggleCheckedChoice = (e) => {
+		setCheckedChoice(e.target.id)
+	}
 	useEffect(() => {
+		if (singleChoice) return onChange(checkedChoice)
 		onChange(checkedList)
-	}, [checkedList])
+	}, [checkedList, checkedChoice])
 
 	return (<nav className={orientation}>
 		{items.map(item => <div
 			className={`menuItem
-			${checkedList.includes(item.type) ? 'checked' : 'unchecked'}
+			${singleChoice ?
+					checkedChoice === item.type ? 'checked' : 'unchecked'
+					: checkedList.includes(item.type) ? 'checked' : 'unchecked'}
 			`}
 			key={item.type}
 			id={item.type}
-			onClick={toggleCheck}
+			onClick={singleChoice ? toggleCheckedChoice : toggleChecklist}
 		>
 			{item.name}
 		</div>)}
