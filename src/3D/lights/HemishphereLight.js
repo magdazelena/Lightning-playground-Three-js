ba2a865c83c21283f9c1f026a8a13bacaa1fadb1
magdazelena <magda.zelezik@gmail.com>
2021-12-11 16:07:30 +0100

    Hemisphere color picker

--- src/3D/lights/HemishphereLight.js
+++ src/3D/lights/HemishphereLight.js
@@ -1,13 +1,25 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import THREE from '../three'
-import { useHelper } from '@react-three/drei'
+import { Html, useHelper } from '@react-three/drei'
 import color from '../../utils/colors.json'
+import { ChromePicker } from 'react-color'
 
 const HemisphereLight = (props) => {
 	const { enableHelper } = props
+	const [mainColor, setColor] = useState(color.powder)
+	const [groundColor, setGroundColor] = useState(color.dark)
 	const light = useRef()
 	useHelper(enableHelper ? light : {}, THREE.HemisphereLightHelper, 5, 'white')
-	return (<hemisphereLight ref={light} color={color.powder} groundColor={color.dark} />)
+	return (<>
+		<hemisphereLight ref={light} color={mainColor} groundColor={groundColor} />
+		<Html as='div' center className='hemispherelight lightcolor'>
+			<p>Hemisphere base color</p>
+			<ChromePicker color={mainColor} onChange={color => setColor(color.hex)} />
+			<p>Hemisphere ground color</p>
+			<ChromePicker color={groundColor} onChange={color => setGroundColor(color.hex)} />
+		</Html>
+	</>
+	)
 }
 
 export default HemisphereLight
