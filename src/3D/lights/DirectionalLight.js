570cb8e9040f03245f7f6823a484a216b3362b15
magdazelena <magda.zelezik@gmail.com>
2021-12-16 21:39:32 +0100

    Added position to directional light

--- src/3D/lights/DirectionalLight.js
+++ src/3D/lights/DirectionalLight.js
@@ -8,13 +8,16 @@ const DirectionalLight = (props) => {
   const { enableHelper, modify } = props
   const [mainColor, setColor] = useState(color.powder)
   const [intensity, setIntensity] = useState(100)
+  const [position, setPosition] = useState([0, 1, 0])
   const light = useRef()
   useHelper(enableHelper ? light : {}, THREE.DirectionalLightHelper, 5, 'yellow')
-  return (<><directionalLight ref={light} color={mainColor} intensity={intensity} />
+  return (<><directionalLight ref={light} color={mainColor} intensity={intensity} position={position} />
     {modify && (<LightControls
       name='directional'
       updateMainColor={setColor}
       updateIntensity={setIntensity}
+      updatePosition={setPosition}
+      initialPosition={[0, 1, 0]}
     />)}
   </>)
 }
